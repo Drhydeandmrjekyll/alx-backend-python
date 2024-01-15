31764a19fac5bd9044e9f010903f45ad1864f116
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-01-15 12:25:50 +0000

    tasks

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,8 +4,8 @@ Module with measure_time function.
 """
 
 import time
-from typing import List
-from asyncio import run
+from typing import List, Callable
+from asyncio import run, gather
 from concurrent.futures import ProcessPoolExecutor
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
@@ -13,24 +13,23 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """
-    Measures total execution time for wait_n(n, max_delay)
+    Measures the total execution time for wait_n(n, max_delay)
     and returns total_time / n.
 
     Args:
         n (int): Number of times to spawn wait_random.
-        max_delay (int): The maximum delay in seconds.
+        max_delay (int): Maximum delay in seconds.
 
     Returns:
-        float: The average execution time per iteration.
+        float: Average execution time per iteration.
     """
     start_time = time.time()
 
     async def run_wait_n():
         return await wait_n(n, max_delay)
 
-    # Run async function in separate process to bypass Global Interpreter Lock
-    with ProcessPoolExecutor() as executor:
-        delays: List[float] = run(executor.submit(run_wait_n))
+    # Runs async function using run
+    delays: List[float] = run(run_wait_n())
 
     end_time = time.time()
     total_time = end_time - start_time
---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module with task_wait_n function.
+"""
+
+import asyncio
+from typing import List, Callable
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous function that spawns task_wait_random n times with
+    specified max_delay.
+
+    Args:
+        n (int): The number of times to spawn task_wait_random.
+        max_delay (int): The maximum delay in seconds.
+
+    Returns:
+        List[float]: List of delays in ascending order.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    delays.sort()
+    return delays
+
+
+if __name__ == "__main__":
+    n = 5
+    max_delay = 6
+    print(asyncio.run(task_wait_n(n, max_delay)))
