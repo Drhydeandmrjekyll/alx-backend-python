b77b666c9d33c7bdd64247452632d1a12fac8766
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-01-16 11:06:26 +0000

    finis

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,11 +8,16 @@ import random
 from typing import AsyncGenerator
 
 
+async def numbers(limit: int) -> AsyncGenerator[float, None]:
+    for _ in range(limit):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     Coroutine that yields random number between 0 and 10 after
     waiting 1 second, repeated 10 times.
     """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+    async for num in numbers(10):
+        yield num
