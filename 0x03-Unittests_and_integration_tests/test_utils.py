4d93419e5bed0755734af22563cbd3a7f757245f
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-02-04 11:55:11 +0000

    parameterize_01

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,45 +4,22 @@ Module containing unit tests for utils.py
 """
 
 import unittest
-from unittest.mock import patch
-from utils import memoize
+from parameterized import parameterized
+from utils import access_nested_map
 
 
-class TestMemoize(unittest.TestCase):
+class TestAccessNestedMap(unittest.TestCase):
     """
-    Test cases for memoize decorator
+    Test cases for access_nested_map function
     """
 
-    class TestClass:
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
         """
-        Test class with a_method and a_property decorated with memoize
+        Test access_nested_map function with different inputs
         """
-
-        def a_method(self):
-            return 42
-
-        @memoize
-        def a_property(self):
-            return self.a_method()
-
-    def test_memoize(self):
-        """
-        Test memoize decorator
-        """
-        # Create instance of TestClass
-        test_instance = self.TestClass()
-
-        # Patch a_method to return expected value
-        with patch.object(test_instance, 'a_method') as mock_a_method:
-            mock_a_method.return_value = 42
-
-            # Call a_property twice
-            result1 = test_instance.a_property
-            result2 = test_instance.a_property
-
-            # Assert that a_method is called once
-            mock_a_method.assert_called_once()
-
-            # Assert that results are correct
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
